64701d05c337a0d59801f054d9bb28f92a35ca25
robertjankowski <robjanw11@wp.pl>
2019-05-21 20:36:28 +0200

    add check in cmd argument

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -3,6 +3,10 @@ package client;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Specify IP as argument");
+            System.exit(-1);
+        }
         Client udpClient = new Client(args[0]);
         udpClient.runClient();
     }
